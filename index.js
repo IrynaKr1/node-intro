5e691233710f32ae71e59b6117b4561b4103b8ea
IrynaKr1 <krutsiryna@gmail.com>
2025-12-28 13:31:42 +0100

    Add method read Directory

--- index.js
+++ index.js
@@ -5,12 +5,29 @@ fs.readdir('.', (err, data) => {
     console.log('err', err);
   } else {
     console.log('readdir data', data);
+    console.log('--------------------');
   }
 });
 
 try {
   const files = fs.readdirSync('.', { encoding: 'utf-8' });
   console.log('readdirSync data', files);
+  console.log('--------------------');
 } catch (err) {
   console.log('err', err);
 }
+
+//Використати також версію даного метода на промісах.
+const fsPromises = require('fs/promises');
+
+async function readDirectory () {
+  try {
+    const dirFiles = await fsPromises.readdir('.');
+    console.log('Wirh promises dirFiles', dirFiles);
+    console.log('--------------------');
+  } catch (err) {
+    console.log('err', err);
+  }
+}
+
+readDirectory();
